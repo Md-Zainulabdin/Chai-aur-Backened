82b4df2194e906fa9e8b4a18fc3f097c14c3762b
Muhammad Zain-ul-Abdin Ansari <zainzabid4338@gmail.com>
2023-11-07 16:08:27 +0500

    setup initial middleware for handling data

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import express from "express"
 import dotenv from 'dotenv'
+import cors from "cors"
 
-dotenv.config()
+// configure dotenv 
+dotenv.config({
+    path: "./env"
+})
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 5000 || process.env.PORT;
+
+// adding middleware for handling upcoming data
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: false, limit: "16kb" }))
 
 app.listen(PORT, () => console.log(`Server is listening on PORT: ${PORT}`))
