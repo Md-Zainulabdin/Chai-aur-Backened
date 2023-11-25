797dc3eb552f6e1262783ee5a489ed6bd5fcffbe
Muhammad Zain-ul-Abdin Ansari <zainzabid4338@gmail.com>
2023-11-25 12:05:32 +0500

    Create Video modal

--- src/models/user.modal.js
+++ src/models/user.modal.js
@@ -1,4 +1,5 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt"
 
 export const userSchema = new Schema({
     username: {
@@ -7,5 +8,44 @@ export const userSchema = new Schema({
         trim: true,
         lowercase: true,
         unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+    fullName: {
+        type: String,
+        required: true,
+        trim: true,
+        index: true
+    },
+    avatar: {
+        type: String, // cloudinary url
+        required: true
+    },
+    coverImage: {
+        type: String
+    },
+    watchHistory: [{
+        type: Schema.Types.ObjectId,
+        ref: "Video"
+    }],
+    password: {
+        type: String,
+        required: [true, "Password is required"]
+    },
+    refreshToken: {
+        type: String
     }
+},
+    {
+        timestamps: true
+    });
+
+userSchema.pre("save", function() {
+    
 })
+export const User = mongoose.model("User", userSchema)
